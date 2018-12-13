56eaeca094b48867abedb9cb927a7c225222e332
Brian Chiware <kmanatorj@gmail.com>
2018-12-13 17:12:01 +0200

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources:rates
-  root 'rates#index'
+  #root 'rates#index'
   resources:markets
-  root 'markets#index'
+  #root 'markets#index'
   resources:currencies
-  root 'currencies#index'
+  #root 'currencies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
